0ad7bb4111f45f5a243bf88138810b3d9c069dca
Hank <ssc9309@hotmail.com>
2016-12-09 20:44:56 -0500

    asdf

---
+++ 192_168_7_77___21025/default/rangeMinerModule.js
@@ -0,0 +1,13 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('rangeMinerModule');
+ * mod.thing == 'a thing'; // true
+ */
+
+module.exports = function(creep)
+{
+    creep.say("RB");
+};
